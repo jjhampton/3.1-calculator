258fa546caf01b0143981123a211a12765068707
John Hampton <john_j_hampton@yahoo.com>
2015-05-19 00:23:43 -0400

    Add decimalPressed function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,6 @@
 //Function to run when window is loaded in browser  Sets variables, event handlers, and contains event listener definitions.
-
-
 var init = function () {
+  //"use strict";
 
   //Declare local variables
   var equalsButton; //for querySelector
@@ -11,11 +10,11 @@ var init = function () {
   var percentButton;//for querySelector
   var $displayArea; //for querySelector, property will be modified by setDisplayArea function
   var calculation = 0; //Number storing result of current calculation
-  var displayedValue = 0; //String storing current display value
+  var displayedValue = "0"; //String storing current display value
 
   var buttonPressedLast; //Stores value of last-pressed button --*** CHANGE TO OBJECT PROPERTY***
   var pendingOperation; //Stores value of last pressed operator button
-  var lastCalculatedInputvalue; //Stores last number value calculated for later use
+  var lastCalculatedInputValue; //Stores last number value calculated for later use
 
 
   /*var wasNumberPressedLast = false;
@@ -44,6 +43,7 @@ var init = function () {
 
   //Function that sets value of calculation value.  Parameter inputValue is used in arithmetic with pendingOperation to generate new calculation value, or when no operation needed, to replace calculation value.
   var setCalculation = function(inputValue) {
+    /*Refactor to use pendingOperation as parameter*/
     switch (pendingOperation) {
       case "+":
         calculation += inputValue;
@@ -60,8 +60,11 @@ var init = function () {
       case "replace":
         calculation = inputValue;
         break;
+      //case "decimal-addon":
+        //calculation = calculation + inputValue;
     }
     lastCalculatedInputValue = inputValue;
+    console.log("LCIV is: " + lastCalculatedInputValue);
   };
 
   //Event handler for clicked number button
@@ -74,22 +77,28 @@ var init = function () {
     switch (buttonPressedLast)  {
       case undefined:
       case "clear":
-        pendingOperation = "+";
         setDisplayArea(textString, false);
+        pendingOperation = "+";
         setCalculation(textNumber);
         break;
       case "number":
         setDisplayArea(textString, true);
-        setCalculation(textNumber);
+        pendingOperation = "replace";
+        setCalculation(lastCalculatedInputValue * 10 +  textNumber);
         break;
       case "decimal":
         setDisplayArea(textString, true);
-        setCalculation(textNumber);
+        pendingOperation = "+";
+        setCalculation(textNumber * 0.1);
         break;
       case "operator":
         setDisplayArea(textString, false);
         setCalculation(textNumber);
         break;
+      case "decimalAfterOperator":
+      setDisplayArea(textString, true);
+      setCalculation(Number(displayedValue));
+      break;
       }
     buttonPressedLast = "number";
     console.log("calculation is: " + calculation);
@@ -154,9 +163,20 @@ var init = function () {
   var decimalPressed = function(event) {
     var button = event.target;
     var text = button.textContent;
+    console.log(text + " CLICKED");
+
 
     if (buttonPressedLast === "number" || buttonPressedLast === undefined) {
-
+      if (displayedValue.indexOf(".") === -1) {
+        setDisplayArea(text, true);
+        pendingOperation = "replace";
+        setCalculation(Number(calculation.toFixed(1)));
+      }
+      buttonPressedLast = "decimal";
+    }
+    if (buttonPressedLast === "operator") {
+      setDisplayArea("0" + text, false);
+      buttonPressedLast = "decimalAfterOperator";
     }
 
     console.log("calculation is: " + calculation);
@@ -174,16 +194,6 @@ var init = function () {
     }
 
   };
-
-  // Using querySelector to assign variables to DOM elements
-  equalsButton = document.querySelector("#button-equal");
-  clearButton = document.querySelector("#button-clear");
-  displayArea = document.querySelector(".display-digits");
-  toggleSignButton = document.querySelector("#button-toggle-sign");
-  decimalButton = document.querySelector("#button-decimal");
-  percentButton = document.querySelector("#button-percent");
-
-
 
   // Loop over every element in document. Finds every element w. matching CSS class and binds event listener to 'click' event on that button.  When element is clicked, function alertButtonValue is called.
   [].forEach.call(document.querySelectorAll('.button-number'), function(element){
